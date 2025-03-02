29c265c409174c752587ca6cd160978dc1d29ad7
emanameen9982773 <emanothard15@gmail.com>
2025-03-03 00:24:36 +0300

    PQ classes

--- Scheduler.java
+++ Scheduler.java
@@ -5,13 +5,15 @@ public class Scheduler{
 
   public static void main(String[] args) {
    
-     PriorityQueue arrival = new PriorityQueue<Process>();
+     PriorityQueue arrival = new PriorityQueue<Integer>();
      PriorityQueue eventQueue= new PriorityQueue<>();
 
      int time=0;
+     int numOfCS=0;
      for(int i=0; i<4; i++)
      System.out.println(arrival.remove());
     while (!arrival.isEmpty() || !eventQueue.isEmpty()) {
+      if(time==arrival.element())
       
       
     }

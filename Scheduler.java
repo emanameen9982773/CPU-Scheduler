320e99f68875cc8d3a21d4b0afb61609466a34df
emanameen9982773 <emanothard15@gmail.com>
2025-03-03 23:57:05 +0300

    update

--- Scheduler.java
+++ Scheduler.java
@@ -6,14 +6,16 @@ public class Scheduler{
   public static void main(String[] args) {
    
      PriorityQueue arrival = new PriorityQueue<Integer>();
-     PriorityQueue eventQueue= new PriorityQueue<>();
+     PriorityQueue eventQueue= new PriorityQueue<Integer>();
 
      int time=0;
      int numOfCS=0;
      for(int i=0; i<4; i++)
      System.out.println(arrival.remove());
     while (!arrival.isEmpty() || !eventQueue.isEmpty()) {
-      if(time==arrival.element())
+      if(time==(int) arrival.element())
+        eventQueue.add(arrival.remove());
+         
       
       
     }

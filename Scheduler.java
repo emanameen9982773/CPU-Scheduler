aed09bb1c23258354fa2600b0956962f23967f3c
emanameen9982773 <emanothard15@gmail.com>
2025-03-04 04:55:11 +0300

    formatting the code

--- Scheduler.java
+++ Scheduler.java
@@ -1,77 +1,82 @@
 import java.util.PriorityQueue;
 import java.util.LinkedList;
-import java.util.List;
 
 public class Scheduler {
 
   public static void main(String[] args) {
 
-    PriorityQueue <Process> arrival = new PriorityQueue<>(Process.arrivalComparator);
-    PriorityQueue <Process> eventQueue = new PriorityQueue<>(Process.burstTimeComparator);
-    LinkedList <Process> termenated = new LinkedList<>();
-    
-    arrival.add(new Process(0,8));
-    arrival.add(new Process(1,4));
-    arrival.add(new Process(2,5));
-    arrival.add(new Process(3,5));
+    PriorityQueue<Process> arrival = new PriorityQueue<>(Process.arrivalComparator);
+    PriorityQueue<Process> eventQueue = new PriorityQueue<>(Process.burstTimeComparator);
+    LinkedList<Process> termenated = new LinkedList<>();
+
+    arrival.add(new Process(0, 8));
+    arrival.add(new Process(1, 4));
+    arrival.add(new Process(2, 5));
+    arrival.add(new Process(3, 5));
 
     int time = 0;
     int numOfCS = 0;
-    int start=0;
-    int cpuI=0;
+    int start = 0;
+    int cpuI = 0;
+
+    Process running = null;
+    while (!arrival.isEmpty() || !eventQueue.isEmpty() || running != null) {
+      
+
+      while (!arrival.isEmpty() && time == arrival.peek().arrival)
+        eventQueue.add(arrival.poll());
 
 
-
-    Process running=null;
-    while (!arrival.isEmpty() || !eventQueue.isEmpty() || running!=null) {
-
-      while (!arrival.isEmpty() && time == arrival.peek().arrival) 
-        eventQueue.add(arrival.poll()); 
-
-      if(running==null&& !eventQueue.isEmpty()){
-        if(time>0){
-        System.out.println(time + "-" + (time+1)  + "\tCS");  // Context Switch
-        time++;}
+      if (running == null && !eventQueue.isEmpty()) {
+        if (time > 0) {
+          System.out.println(time + "-" + (time + 1) + "\tCS"); // Context Switch
+          time++;
+        }
         numOfCS++;
-        running=eventQueue.remove();
-        start=time;
+        running = eventQueue.remove();
+        start = time;
         continue;
       }
 
-      else if(running!=null && !eventQueue.isEmpty())
-      if( running.burstTime>eventQueue.element().burstTime){
-        System.out.println(start+"-"+time+"\tP"+running.ID);
-        System.out.println(time+"-"+(time+1)+"\tCS");
-        numOfCS++;
-        time++;
-        eventQueue.add(running);
-        running=eventQueue.remove();
-        start =time;
-        continue;
-      }
-    
+
+      else if (running != null && !eventQueue.isEmpty())
+        if (running.burstTime > eventQueue.element().burstTime) {
+          System.out.println(start + "-" + time + "\tP" + running.ID);
+          System.out.println(time + "-" + (time + 1) + "\tCS");
+          numOfCS++;
+          time++;
+          eventQueue.add(running);
+          running = eventQueue.remove();
+          start = time;
+          continue;
+        }
+
+
+
       if (running != null) {
-      if(running.first){
-        running.startTime=time;
-        running.first=false;
+
+        if (running.first) {
+          running.startTime = time;
+          running.first = false;
+        }
+
+        running.burstTime--;
+        time++;
+
+        if (running.burstTime == 0) {
+          running.termenationTime = time;
+          termenated.add(running);
+          System.out.println(start + "-" + time + "\tP" + running.ID);
+          running = null;
+        }
+        continue;
       }
-      running.burstTime--;
+
+
+      cpuI++;
       time++;
-      if (running.burstTime==0) {
-        running.termenationTime=time;
-        termenated.add(running);
-        System.out.println(start+"-"+time+"\tP"+running.ID);
-        running=null;
-        
-      }
-      continue;
-    }
-     cpuI++;
-     time++;
-
 
     }
-     
 
   }
 

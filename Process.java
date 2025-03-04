587fb23b1020f74efc09d217dad074a41ca8b6bc
emanameen9982773 <emanothard15@gmail.com>
2025-03-04 04:48:23 +0300

    impelmentaion of scheduler

--- Process.java
+++ Process.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Process {
   
     public static int counter;
@@ -7,16 +9,33 @@ public class Process {
     public int arrival;
     public int burstTime;
     public Process nextProcess;
+    public boolean first;
+    
 
     public Process(int arr, int burst){
 
-        ID=counter++;
+        ID=++counter;
         arrival=arr;
         burstTime=burst;
         nextProcess=null;
+        first=true;
  
     }
 
+      public static Comparator<Process> arrivalComparator = new Comparator<Process>() {
+        public int compare(Process p1, Process p2) {
+            return Integer.compare(p1.arrival, p2.arrival);
+        }
+    };
     
+
+    public static Comparator<Process> burstTimeComparator = new Comparator<Process>() {
+        public int compare(Process p1, Process p2) {
+            if (p1.burstTime == p2.burstTime) {
+                return Integer.compare(p1.arrival, p2.arrival); // FCFS for ties
+            }
+            return Integer.compare(p1.burstTime, p2.burstTime);
+        }
+    };
     
 }
--- Scheduler.java
+++ Scheduler.java
@@ -1,25 +1,77 @@
-import java.util.PriorityQueue;;
+import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Scheduler{
-    
+public class Scheduler {
 
   public static void main(String[] args) {
-   
-     PriorityQueue arrival = new PriorityQueue<Integer>();
-     PriorityQueue eventQueue= new PriorityQueue<Integer>();
 
-     int time=0;
-     int numOfCS=0;
-     for(int i=0; i<4; i++)
-     System.out.println(arrival.remove());
-    while (!arrival.isEmpty() || !eventQueue.isEmpty()) {
-      if(time==(int) arrival.element())
-        eventQueue.add(arrival.remove());
-         
-      
-      
+    PriorityQueue <Process> arrival = new PriorityQueue<>(Process.arrivalComparator);
+    PriorityQueue <Process> eventQueue = new PriorityQueue<>(Process.burstTimeComparator);
+    LinkedList <Process> termenated = new LinkedList<>();
+    
+    arrival.add(new Process(0,8));
+    arrival.add(new Process(1,4));
+    arrival.add(new Process(2,5));
+    arrival.add(new Process(3,5));
+
+    int time = 0;
+    int numOfCS = 0;
+    int start=0;
+    int cpuI=0;
+
+
+
+    Process running=null;
+    while (!arrival.isEmpty() || !eventQueue.isEmpty() || running!=null) {
+
+      while (!arrival.isEmpty() && time == arrival.peek().arrival) 
+        eventQueue.add(arrival.poll()); 
+
+      if(running==null&& !eventQueue.isEmpty()){
+        if(time>0){
+        System.out.println(time + "-" + (time+1)  + "\tCS");  // Context Switch
+        time++;}
+        numOfCS++;
+        running=eventQueue.remove();
+        start=time;
+        continue;
+      }
+
+      else if(running!=null && !eventQueue.isEmpty())
+      if( running.burstTime>eventQueue.element().burstTime){
+        System.out.println(start+"-"+time+"\tP"+running.ID);
+        System.out.println(time+"-"+(time+1)+"\tCS");
+        numOfCS++;
+        time++;
+        eventQueue.add(running);
+        running=eventQueue.remove();
+        start =time;
+        continue;
+      }
+    
+      if (running != null) {
+      if(running.first){
+        running.startTime=time;
+        running.first=false;
+      }
+      running.burstTime--;
+      time++;
+      if (running.burstTime==0) {
+        running.termenationTime=time;
+        termenated.add(running);
+        System.out.println(start+"-"+time+"\tP"+running.ID);
+        running=null;
+        
+      }
+      continue;
     }
+     cpuI++;
+     time++;
 
+
+    }
+    System.out.println(numOfCS);
 
   }
 

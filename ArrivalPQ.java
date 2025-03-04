6143dc4fd5941f8e20d781761f13527fe3912fb8
laura-almasoud <lauraalmasoud@hotmail.com>
2025-03-04 22:58:58 +0300

    Update ArrivalPQ.java

--- ArrivalPQ.java
+++ ArrivalPQ.java
@@ -1,3 +1,28 @@
+import java.util.PriorityQueue;
+
 public class ArrivalPQ {
-    
+    private PriorityQueue<Process> queue;
+
+    public ArrivalTimePQ() {
+        queue = new PriorityQueue<>(Process.arrivalComparator);
+    }
+
+    public void addProcess(Process process) {
+        queue.offer(process);
+    }
+
+    public Process getNextProcess() {
+        return queue.poll();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public void printQueue() {
+        System.out.println("Arrival Time Priority Queue:");
+        for (Process p : queue) {
+            System.out.println(p);
+        }
+    }
 }
